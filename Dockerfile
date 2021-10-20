a31283b78e3c571b5cd6134773ce1efdb982b6c6
xsierra <xavier.sierra@gmail.com>
2021-10-20 23:40:28 +0200

    feat: change running images to alpine so it can run on AMD

--- Dockerfile
+++ Dockerfile
@@ -6,22 +6,7 @@ RUN curl https://dlcdn.apache.org/maven/maven-3/3.8.3/binaries/apache-maven-3.8.
 RUN tar xf /tmp/apache-maven-3.8.3.tar.gz -C /opt
 RUN ln -s /opt/apache-maven-3.8.3 /opt/maven
 ENV PATH="/opt/maven/bin:${PATH}"
-# Installing musl
-RUN curl https://musl.libc.org/releases/musl-1.2.2.tar.gz --output /tmp/musl-1.2.2.tar.gz
-RUN tar xf /tmp/musl-1.2.2.tar.gz -C /opt/
-WORKDIR /opt/musl-1.2.2
-RUN ./configure --disable-shared --prefix=/usr/local/musl
-RUN make
-RUN make install
-ENV PATH="/usr/local/musl/bin:${PATH}"
-# Installing zlib
-RUN curl https://zlib.net/zlib-1.2.11.tar.gz --output /tmp/zlib-1.2.11.tar.gz
-RUN tar xf /tmp/zlib-1.2.11.tar.gz -C /opt/
-WORKDIR /opt/zlib-1.2.11/
-ENV CC="musl-gcc"
-RUN ./configure --static --prefix=/usr/local/musl
-RUN make
-RUN make install
+
 ENV LD_LIBRARY_PATH="/usr/lib:${LD_LIBRARY_PATH}"
 ENV PKG_CONFIG_PATH="/usr/lib/pkgconfig:${PKG_CONFIG_PATH}"
 WORKDIR /radioyerevan
--- build.sh
+++ build.sh
@@ -1,10 +1,33 @@
-#!/bin/sh
+#!/usr/bin/env bash
+optspec=":hv-:"
 RADIO_YEREVAN_VERSION=$( mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec )
+SOURCE_PATH=$(pwd)
 
+while getopts "$optspec" optchar; do
+  case "${optchar}" in
+    -)
+      case "${OPTARG}" in
+        sourcepath)
+          val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          SOURCE_PATH=${val}
+          ;;
+        *)
+          if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
+              echo "Unknown option --${OPTARG}" >&2
+          fi
+          ;;
+      esac;;
+    *)
+      if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then
+          echo "Non-option argument: '-${OPTARG}'" >&2
+      fi
+      ;;
+  esac
+done
 echo "Building native image builder"
-docker build -t radio-yerevan-builder .
+docker build --no-cache -t radio-yerevan-builder .
 echo "Compiling radioyerevan version $RADIO_YEREVAN_VERSION"
-docker run --rm -v "D:/workspaces/radioyerevan:/radioyerevan" radio-yerevan-builder mvn clean package
+docker run --rm -v "$SOURCE_PATH:/radioyerevan" radio-yerevan-builder mvn clean package
 
 echo "Building postgres migrator docker image"
 docker build \
